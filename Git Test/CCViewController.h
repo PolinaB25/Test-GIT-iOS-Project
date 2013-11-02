e56679dbd8a595b20ee885228090bf2c2a2fc4dc
Polina Bates <Polina@Polinas-MacBook-Pro.local>
2013-11-02 12:34:37 -0500

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Polina Boldyreva on 11/2/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Polina Boldyreva on 11/2/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
