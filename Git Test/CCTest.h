2aced0a7136b9efe4d1eb293ff4a37869ae8759f
Polina Bates <Polina@Polinas-MacBook-Pro.local>
2013-11-02 13:29:34 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Polina Boldyreva on 11/2/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Polina Boldyreva on 11/2/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
